4f85feab60dd34ea610bf7780d10e49ce5acdc62
fvafrCU <fvafrcu@arcor.de>
2018-01-18 17:31:51 +0100

    fix runit

--- tests/runit.R
+++ tests/runit.R
@@ -10,22 +10,22 @@ is_failure <- function(result) {
 if (interactive()) {
     devtools::load_all(pkg = ".") # needed to use devtools' shim version of
     # base's system.file
-    unit_dir <- system.file("inst", "runit_tests", package = "asciidoc")
+    unit_dir <- system.file("inst", "runit_tests", package = "rasciidoc")
 } else {
-    require("asciidoc", quietly = TRUE, character.only = TRUE) ||
+    require("rasciidoc", quietly = TRUE, character.only = TRUE) ||
         stop("package '", pkgname, "' not found")
     r_call <- commandArgs(trailingOnly = FALSE)
     if (any(grepl("--file", r_call))) {
         unit_dir <- file.path("inst", "runit_tests")
     } else {
-        unit_dir <- system.file("runit_tests", package = "asciidoc")
+        unit_dir <- system.file("runit_tests", package = "rasciidoc")
     }
 }
 if (! dir.exists(unit_dir)) {
     stop("Can not find RUnit test directory ", unit_dir,
          ". Try to (re)install the package first.")
 }
-package_suite <- RUnit::defineTestSuite("asciidoc_unit_test",
+package_suite <- RUnit::defineTestSuite("rasciidoc_unit_test",
                                         dirs = unit_dir,
                                         testFileRegexp = "^.*\\.[rR]",
                                         testFuncRegexp = "^test_+")
