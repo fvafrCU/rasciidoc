266a87b15785a1bf108c5d1f2637eddbd120f878
fvafrCU <fvafrcu@arcor.de>
2018-03-09 16:25:03 +0100

    improve coverage

---
+++ inst/runit_tests/runit-highr_internals.R
@@ -0,0 +1,24 @@
+if (interactive()) devtools::load_all()
+test_try_parse <- function() {
+    RUnit::checkTrue(rasciidoc:::try_parse("asdf"))
+    RUnit::checkTrue(! rasciidoc:::try_parse("a(df"))
+}
+
+test_escape_latex <- function() {
+    RUnit::checkIdentical(rasciidoc:::escape_latex("\\{"),
+                          "\\textbackslash{}\\{")
+}
+
+test_escape_html <- function() {
+    RUnit::checkIdentical(rasciidoc:::escape_html("& < > \""),
+                          "&amp; &lt; &gt; &quot;")
+}
+
+test_group_src <- function()  {
+    # adapted from highr/tests/testit/test-utils.R
+    RUnit::checkIdentical(rasciidoc:::group_src('1+1'), list('1+1'))
+    RUnit::checkIdentical(rasciidoc:::group_src(c('1+1+', '1')),
+                          list(c('1+1+', '1')))
+    RUnit::checkIdentical(rasciidoc:::group_src(c('1+1+', '1', 'TRUE')),
+                          list(c('1+1+', '1'), 'TRUE'))
+}
